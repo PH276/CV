7ce67f1fc37948adf3335506e219aa1e59df1632
PH276 <pascal.huitorel@lepoles.com>
2017-10-06 13:02:25 +0200

    commit6
    recuperation de donnees de la BDD

---
+++ admin/connexion.php
@@ -0,0 +1,2 @@
+<?php
+$pdo = new PDO("mysql:host=localhost;dbname=Pascal_CV", 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,16 @@
+<?php include('connexion.php'); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin : Prénom nom</title>
+    <?php     $resultat = $pdo->query("SELECT * FROM t_utilisateurs");
+        $utilisateur = $resultat->fetch(PDO::FETCH_ASSOC);
+ ?>
+    <title>Admin : <?= $utilisateur['pseudo'] ?></title>
 </head>
 <body>
-    <h1>Admin</h1>
+    <h1>Admin <?= $utilisateur['prenom'].' '.$utilisateur['nom'] ?></h1>
 </body>
 </html>
