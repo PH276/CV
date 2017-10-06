4506e3f1e77be7a61f2565f5f5dd291f9e9ca6f3
PH276 <pascal.huitorel@lepoles.com>
2017-10-06 14:56:39 +0200

    commit7
    contrôle des données de la requête (admin/index.php)

--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,22 @@
-<?php include('connexion.php'); ?>
+<?php include('connexion.php');
+$resultat = $pdo->query("SELECT * FROM t_utilisateurs");
+$utilisateur = $resultat->fetch(PDO::FETCH_ASSOC);
+$utilisateurOK = false;
+if ($resultat->rowCount()>0){
+    $utilisateurOK = true;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <?php     $resultat = $pdo->query("SELECT * FROM t_utilisateurs");
-        $utilisateur = $resultat->fetch(PDO::FETCH_ASSOC);
- ?>
-    <title>Admin : <?= $utilisateur['pseudo'] ?></title>
+    <title>Admin : <?= ($utilisateurOK)?$utilisateur['pseudo']:''; ?></title>
 </head>
 <body>
-    <h1>Admin <?= $utilisateur['prenom'].' '.$utilisateur['nom'] ?></h1>
+    <h1>Admin <?= ($utilisateurOK)?($utilisateur['prenom'].' '.$utilisateur['nom']):'prénom nom';
+     ?></h1>
 </body>
 </html>
